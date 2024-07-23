297617ab441e171565a21d7ac0a816c9973af793
ismailylmz31 <64439554+ismailylmz31@users.noreply.github.com>
2024-07-23 14:27:33 +0300

    image url hatası düzeltme

--- menus/models.py
+++ menus/models.py
@@ -18,7 +18,7 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
-    image = models.ImageField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.product.name
