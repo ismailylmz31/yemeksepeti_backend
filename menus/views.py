b3422fb4a10cba32d07b8da6dc7c097d61abbe4a
ismailylmz31 <64439554+ismailylmz31@users.noreply.github.com>
2024-07-24 16:25:31 +0300

    menu search

--- menus/views.py
+++ menus/views.py
@@ -5,12 +5,14 @@ from .serializers import MenuSerializer
 
 class MenuListCreate(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
-    
+    serializer_class = MenuSerializer    
 
     def perform_create(self, serializer):
         restaurant = self.request.data.get('restaurant')
         serializer.save(restaurant_id=restaurant)
+
+    search_fields = ['name', 'description']
+    ordering_fields = ['name',]    
 
 class MenuDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
@@ -21,4 +23,7 @@ class RestaurantMenusView(generics.ListAPIView):
         restaurant_id = self.kwargs['pk']
         return Menu.objects.filter(restaurant_id=restaurant_id)
     
+    search_fields = ['name', 'description']
+    ordering_fields = ['name',]  
+
     serializer_class = MenuSerializer
